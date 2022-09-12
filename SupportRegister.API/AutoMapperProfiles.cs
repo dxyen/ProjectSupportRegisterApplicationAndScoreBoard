818dec2134e1aa84a41f6cc7ef2e77f9b92a8bce
Đỗ Xuân Yên <75532626+dxyen@users.noreply.github.com>
2022-09-12 13:52:59 +0700

    Add API Users

---
+++ SupportRegister.API/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SupportRegister.Data.Models;
+using SupportRegister.ViewModels.Requests.System.Users;
+using SupportRegister.ViewModels.ViewModels;
+
+namespace SupportRegister.API
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            #region User
+            CreateMap<AppUser, UserViewModel>().ReverseMap();
+            CreateMap<AppUser, UserUpdateRequest>().ReverseMap();
+            #endregion
+        }
+    }
+}
--- SupportRegister.ViewModels/Requests/System/Users/RegisterRequest.cs
+++ SupportRegister.ViewModels/Requests/System/Users/RegisterRequest.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SupportRegister.ViewModels.Requests.System.Users
 {
-    internal class RegisterRequest
+    public class RegisterRequest
     {
+        [Required(ErrorMessage = "Please enter username")]
+        [Display(Name = "Username:", Prompt = "Username...")]
+        [RegularExpression("^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$",
+            ErrorMessage = "Username must be 8-20 characters long, does not contain special characters")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please enter password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password:", Prompt = "********")]
+        [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[#$^+=!*()@%&]).{8,}$",
+            ErrorMessage = "Passwords must be at least 6 characters, one digit, one uppercase, one non alphanumeric character")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please enter confirmation password")]
+        [Compare("Password", ErrorMessage = "Password and Confirmation Password must match")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmation Password:", Prompt = "********")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Please enter your full name")]
+        [Display(Name = "Full Name:", Prompt = "Your full name")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Please enter email")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email:", Prompt = "example@example.com")]
+        [RegularExpression("^[a-z][a-z0-9_\\.]{5,32}@[a-z0-9]{2,}(\\.[a-z0-9]{2,4}){1,2}$", ErrorMessage = "Email is invalid")]
+        public string Email { get; set; }
     }
 }
